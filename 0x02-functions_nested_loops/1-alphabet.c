78036da533d7c6f2ab46d2cdd8bfe01ec3c8a36c
happyrw <happy222004567@gmail.com>
2023-09-12 08:46:03 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	for(i='a'; i<='z'; i++)
+		_putchar('i');
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry for compiler
+ *
+ * Return: retuen (0);
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
