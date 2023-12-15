936ef4071f1b0888e694231509ef2d757894e82c
happyrw <happy222004567@gmail.com>
2023-12-15 03:57:50 -0800

    first answer

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts: Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *s)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
